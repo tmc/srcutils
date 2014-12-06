26424d851cd14f718a023d5bec7287cdc7a41257
Travis Cline <travis.cline@gmail.com>
2014-12-05 17:55:46 -0800

    Increase complexity of test code.

--- test/original/main.go
+++ test/original/main.go
@@ -7,3 +7,25 @@ func main() {
 func init() {
 	z(31, "foobar")
 }
+
+func init() {
+	foobar := func() {
+		x(31, "foobar")
+	}
+	foobar()
+}
+
+func init() {
+	new(dummy).test()
+}
+
+type dummy struct{}
+
+func (d *dummy) test() {
+	baz := &struct{ foo func() }{
+		func() {
+			x(7, "oi")
+		},
+	}
+	baz.foo()
+}
